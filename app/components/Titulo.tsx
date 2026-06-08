3d19d98c6c1afa3a0b0de076b17112c6f6cc8026
Drospi <drospigliozi@gmail.com>
2026-06-08 08:55:04 -0400

    Titulo

---
+++ app/components/Titulo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Titulo = () => {
+  return (
+    <div className='flex flex-auto justify-between items-center mb-4'>
+        
+        <h1 className='text-2xl font-bold'>Exercicios</h1>
+        <p className='text-gray-500'>4</p>
+    </div>
+  )
+}
+
+export default Titulo
